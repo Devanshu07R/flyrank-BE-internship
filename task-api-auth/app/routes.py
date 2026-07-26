c91b021aace01042de050d177dfe29cc69a70278
Devanshu07R <devanshudasgupta142004@gmail.com>
2026-07-26 11:49:47 +0530

    Stage 4: Authentication with supabase completed

--- task-api-auth/app/routes.py
+++ task-api-auth/app/routes.py
@@ -12,17 +12,43 @@ router = APIRouter()
 @router.post("/signup")
 def signup(user: UserSignup):
     try:
-        return create_user(user.email, user.password)
+        create_user(user.email, user.password)
+        
+        return{
+            "message: User created successfully"
+        }
+        
     except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
+        raise HTTPException(
+            status_code=400,
+            detail="Unable to create account. Please check your email and try again."
+        )
+        
+    
 @router.post("/login")
 def login(user: UserLogin):
     try:
-        return authenticate_user(user.email, user.password)
+       response = authenticate_user(
+           user.email,
+           user.password
+       )
+       return{
+           "message": "Login successful",
+           "access_token": response.session.access_token,
+           "token_type": "Bearer"
+       }
+       
     except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid email or password."
+        )
+    
 
 @router.get("/me")
 def get_me(current_user=Depends(get_current_user)):
-    return current_user
+    return {
+        "id": current_user.id,
+        "email": current_user.email,
+        "role": current_user.role
+    }
